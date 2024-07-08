df4652c7ab64b628442af0880088a47892e633a0
woobsit <woobsit@gmail.com>
2024-07-08 12:53:13 +0100

    Add logo, inputs to form

--- src/view/Login.js
+++ src/view/Login.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./../App.css";
-import LogoImage from "./../assets/images/logo.png"
+import LogoImage from "./../assets/images/logo.png";
 
 function Login() {
   return (
-    <>
-<nav className="nav">
-    <img src={LogoImage} alt="logo" className="nav__logo" />
-</nav>
+    <div className="container">
+      <div className="form">
+        <div className="form__logo-container">
+        <img src={LogoImage} alt="logo" className="form__logo-image" /></div>
+        <div className="form__box">
+          <h2 className="form__header-one">Student Login</h2>
+          <h6 className="form__header-two">Please sign in to login</h6>
 
-    <div className="form">
-      <div className="form__box">
-        <h1 className="form__header-one">Student Login</h1>
-        <h5 className="form__header-two">Please sign in login</h5>
-
-        <div className="form__inputs-box">
-          <input type="text" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button className="form__button">Login</button>
+          <div className="form__inputs-box">
+            <input type="text" placeholder="email" className="form__input form__input-text"/>
+            <input type="password" placeholder="password" className="form__input form__input-password" />
+            <button className="form__button">Login</button>
+          </div>
         </div>
       </div>
     </div>
-    </>
   );
 }
 
