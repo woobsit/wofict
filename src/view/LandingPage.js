86d284db5c95f3caf83d310f884b742e20f012d0
woobsit <woobsit@gmail.com>
2024-07-10 12:16:37 +0100

    Add validator, Add input validation

--- src/view/LandingPage.js
+++ src/view/LandingPage.js
@@ -1,21 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./../App.css";
 import LogoImage from "./../assets/images/logo.png";
+//Fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faKey, faEye } from "@fortawesome/free-solid-svg-icons";
+//Validor
+import validator from "validator";
 
 function LandingPage() {
+  const [inputFields, setInputFields] = useState({
+    email: "",
+    password: "",
+    remember_token: null,
+  });
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  //Set value of inputs
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setInputFields({ ...inputFields, [name]: newValue });
+  };
+
+  //Validate inputs
+  const validate = (inputValues) => {
+    let errors = {};
+    if (!validator.isEmail(inputValues.email)) {
+      errors.email = "Enter a valid email address";
+    }
+    if (inputValues.password.length < 6) {
+      errors.password = "Password should not be less than 6 characters";
+    }
+    return errors;
+  };
+
+  //Submit form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(inputFields)); //object of errors
+    setSubmitting(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitting) {
+      //finishSubmit();
+      console.log("submitted");
+    }
+  }, [errors]);
+
+  //Show and hide password
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
   return (
     <div className="container">
-      <div className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form__logo-container">
           <Link to="/">
             <img src={LogoImage} alt="logo" className="form__logo-image" />
@@ -29,12 +74,18 @@ function LandingPage() {
             <div className="form__input-box">
               <FontAwesomeIcon icon={faUser} className="form__input-icon" />
               <input
-                type="text"
+                type="email"
                 placeholder="Enter your email/username"
                 className="form__input"
                 required
+                value={inputFields.email}
+                onChange={handleChange}
+                name="email"
               />
             </div>
+            <p className="text-tertiary" id="error-message">
+              <span>{errors.email}</span>
+            </p>
             <div className="form__input-box">
               <FontAwesomeIcon icon={faKey} className="form__input-icon" />
               <input
@@ -43,22 +94,27 @@ function LandingPage() {
                 className="form__input"
                 required
                 value={password}
+                name="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
+              <FontAwesomeIcon
+                icon={faEye}
+                className="form__input-icon--eye"
+                onClick={togglePasswordVisibility}
+              />
             </div>
-            <button
-              type="button"
-              className="toggle-password-visibility"
-              onClick={togglePasswordVisibility}
-            >
-              {showPassword ? "Hide" : "Show"}
-            </button>
+            <p className="text-tertiary" id="error-message">
+              <span>{errors.password}</span>
+            </p>
             <div className="form__checkbox-link-box">
               <div className="form__checkbox-box">
                 <input
                   type="checkbox"
                   className="form__input--checkbox"
                   id="remember"
+                  value={inputFields.remember_token}
+                  onChange={handleChange}
+                  name="remember_token"
                 />
                 <label htmlFor="remember" className="form__checkbox-label">
                   Remember me?
@@ -70,14 +126,13 @@ function LandingPage() {
                 </Link>
               </div>
             </div>
-            <div>
-              <div></div>
-              <div></div>
-            </div>
-            <button className="form__button">Login</button>
+
+            <button className="form__button" disabled={loading}>
+              Login
+            </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
