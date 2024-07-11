e609904329ed74a12d7854f80e056e08b4ec2ca2
woobsit <woobsit@gmail.com>
2024-07-11 08:58:34 +0100

    Add dynamic background

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import "./App.css";
 import LandingPage from "./view/LandingPage";
 import React from "react";
 //React Router package
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
+  const getBackgroundClass = (pathname) => {
+    switch (pathname) {
+      case "/dashboard":
+        return "dashboard-background";
+      case "/about":
+        return "about-background";
+      case "/contact":
+        return "contact-background";
+      default:
+        return "home-background";
+    }
+  };
+
+  const backgroundClass = getBackgroundClass(location.pathname);
+
   return (
-    <Routes>
-      <Route>
-        <Route path="/" element={<LandingPage />} key="landing-page" />
-      </Route>
-    </Routes>
+    <div className={`app ${backgroundClass}`}>
+      <Routes>
+        <Route>
+          <Route path="/" element={<LandingPage />} key="landing-page" />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
