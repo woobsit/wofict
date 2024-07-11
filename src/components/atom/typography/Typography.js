245231a4cd99a36bc61430b3ddb095fc0e4f0289
woobsit <woobsit@gmail.com>
2024-07-11 15:49:52 +0100

    Add typography component

---
+++ src/components/atom/typography/Typography.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./typography.module.css";
+
+const Typography = ({ variant, children, className, ...props }) => {
+  const Tag = variant;
+  return (
+    <Tag
+      className={`${styles.typography} ${styles[variant]} ${className}`}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+Typography.propTypes = {
+  variant: PropTypes.oneOf(["h1", "h2", "h3", "p", "span"]).isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+Typography.defaultProps = {
+  className: "",
+};
+
+export default Typography;
