7c9ec6b0bfc9db53f55ad0fd63a1084871a89277
woobsit <woobsit@gmail.com>
2024-07-19 18:07:07 +0100

    Add forget password, Fix logout

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,6 +4,10 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8000/api", // Set your base URL here
   withCredentials: true, // This allows cookies to be sent with requests
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 export default axiosInstance;
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,7 +1,64 @@
-import React from "react";
-
+import React, { useState } from "react";
+//React route dom
+import { useNavigate } from "react-router-dom";
+//API service
+import authService from "./../api/authService";
+//sweet alert2
+import Swal from "sweetalert2";
+//Spinner loader
+import Loader from "./../components/atom/loader";
+//js-cookies
+import Cookies from "js-cookie";
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  const userLogout = async () => {
+    try {
+      setLoading(true);
+      const response = await authService.userLogout();
+
+      if (response.status === 204) {
+        setLoading(false);
+        Cookies.remove("auth_user_token"); // remove token in cookies
+        navigate("/");
+      } else if (response.status === 401) {
+        setLoading(false);
+        Swal.fire({
+          icon: "error",
+          title: response.message,
+          text: "Unauthorized user",
+        });
+      } else if (response.status === 500) {
+        setLoading(false);
+        Swal.fire({
+          icon: "error",
+          title: response.message,
+          text: "A system error has occurred",
+        });
+      }
+    } catch (error) {
+      setLoading(false);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "An unexpected error occurred. Please try again.",
+      });
+    }
+  };
+
+  return (
+    <>
+      {loading && <Loader />}
+      <div>
+        Home
+        <button onClick={userLogout} disabled={loading}>
+          Logout
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
