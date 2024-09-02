3f4a50a6750aafe81d297ea492ca8c889e4acd78
unknown <ola.ses@yahoo.com>
2024-09-02 04:20:06 +0100

    Add view credentials feature

--- src/view/admin/AdminCredentialsAllInfo.js
+++ src/view/admin/AdminCredentialsAllInfo.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 //Molecule
 import AdminHeader from "../../components/molecule/admin/AdminHeader";
 //Custom component
 import Typography from "../../components/atom/typography/Typography";
+import Button from "react-bootstrap/Button";
 //Fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useParams } from "react-router-dom";
 //API service
 import authService from "../../api/authService";
 import getAuthAdminData from "./../../api/handleAuthAdminCookies";
@@ -16,12 +17,14 @@ import { notify } from "../../utils/Notification";
 function AdminCredentialsAllInfo() {
   const { id } = useParams();
   const { website_info } = getAuthAdminData();
+  const navigate = useNavigate();
 
   const [fetchUserByCredentialsData, setFetchUserByCredentialsData] = useState(
     []
   );
   const [fetchUserByCredentialsStatus, setFetchUserByCredentialsStatus] =
     useState(false);
+  const [pdfUrl, setPdfUrl] = useState(null);
 
   useEffect(() => {
     if (id) {
@@ -43,6 +46,26 @@ function AdminCredentialsAllInfo() {
         );
       } else if (response.status === 500) {
         notify("error", "System Error", response.message);
+      }
+    } catch (error) {
+      notify(
+        "error",
+        "Error",
+        "An unexpected error occurred. Please try again."
+      );
+    }
+  }
+
+  async function fetchCredentials(id) {
+    try {
+      const response = await authService.viewCredentials(id);
+      if (response.status === 200) {
+        const pdfUrl = URL.createObjectURL(
+          new Blob([response.data], { type: "application/pdf" })
+        );
+        setPdfUrl(pdfUrl);
+      } else {
+        throw new Error("Failed to fetch PDF");
       }
     } catch (error) {
       notify(
@@ -105,7 +128,7 @@ function AdminCredentialsAllInfo() {
                     " " +
                     fetchUserByCredentialsData.surname}
                 </Typography>
-                <Typography variant="p" className="user-email">
+                <Typography variant="span" className="user-email">
                   {fetchUserByCredentialsData.email}
                 </Typography>
               </div>
@@ -113,11 +136,192 @@ function AdminCredentialsAllInfo() {
           )}
         </div>
         <div className="user-info-wrapper">
-          <div className="card user-info user-personal-info"></div>
-          <div className="card user-info user-education-info"></div>
-          <div className="card user-info user-other-info"></div>
+          <div className="card user-info user-personal-info">
+            <Typography variant="h4" className="user-text">
+              Personal Info
+            </Typography>
+            <div className="">
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Firstname:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.firstname}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Surname:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.surname}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Other names:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.other_names}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Gender:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.gender}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Date of Birth:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.date_of_birth}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Contact address
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.contact_address}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Phone number:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.phone_number}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  State of Origin:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.state_of_origin}
+                </Typography>
+              </div>
+            </div>
+          </div>
+          <div className="card user-info user-education-info">
+            <Typography variant="h5" className="user-text">
+              Educational Background
+            </Typography>
+            <div className="">
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Course:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  To be set later
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Session:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.class_sessions}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  Qualification Level:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  {fetchUserByCredentialsStatus &&
+                    fetchUserByCredentialsData.qualification_level}
+                </Typography>
+              </div>
+              <div className="user-heading-name">
+                <Typography variant="h6" className="user-heading">
+                  English Language Fluency:
+                </Typography>
+                <Typography variant="span" className="user-name-value">
+                  To be set later
+                </Typography>
+              </div>
+            </div>
+          </div>
+          <div className="card user-info user-other-info">
+            <Typography variant="h5" className="user-text">
+              Other Info
+            </Typography>
+            <div className="user-heading-name">
+              <Typography variant="h6" className="user-heading">
+                Converstion Strenght:
+              </Typography>
+              <Typography variant="span" className="user-name-value">
+                To be set later
+              </Typography>
+            </div>
+            <div className="user-heading-name">
+              <Typography variant="h6" className="user-heading">
+                Conputer Literacy:
+              </Typography>
+              <Typography variant="span" className="user-name-value">
+                To be set later
+              </Typography>
+            </div>
+            <div className="user-heading-name">
+              <Typography variant="h6" className="user-heading">
+                ICT Referral
+              </Typography>
+              <Typography variant="span" className="user-name-value">
+                To be set later
+              </Typography>
+            </div>
+          </div>
+          <div className="card user-button-groups">
+            <div>
+              <Button
+                variant="info"
+                size="sm"
+                className="user__button--view"
+                onClick={() => fetchCredentials(id)} // Call fetchCredentials with id
+              >
+                View Credentials
+              </Button>
+              <Button variant="primary" size="sm">
+                Download Credentials
+              </Button>
+            </div>
+            <div>
+              <Button
+                variant="secondary"
+                size="sm"
+                onClick={() => navigate(-1)}
+              >
+                Back
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
+      {pdfUrl && (
+        <div className="pdf-viewer">
+          <iframe
+            src={pdfUrl}
+            title="PDF Viewer"
+            width="100%"
+            height="500px"
+            style={{ border: "none" }}
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 }
