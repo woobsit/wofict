c3e3290249649cd723039a92338352d0daed2ac8
woobsit <woobsit@gmail.com>
2024-10-04 10:02:23 +0100

    WIP: dashboard

--- src/components/molecule/admin/AdminMain.js
+++ src/components/molecule/admin/AdminMain.js
@@ -266,214 +266,224 @@ function AdminMain() {
         </Card> */}
       </div>
       <div className="cards-container">
-        <div className="cards-container__inner-box">
-          <Card className="card card__card--black">
-            <div className="card-upper">
-              <div className="card-upper__wrapper">
-                <div className="card-upper__upper">Admitted Users</div>
-                <div className="card-upper__lower">
-                  {countCurrentStudentsState ? null : countCurrentStudents}
+        <div className="">
+          <div className="cards-container__inner-box">
+            <Card className="card card__card--black">
+              <div className="card-upper">
+                <div className="card-upper__wrapper">
+                  <div className="card-upper__upper">Admitted Users</div>
+                  <div className="card-upper__lower">
+                    {countCurrentStudentsState ? null : countCurrentStudents}
+                  </div>
+                </div>
+                <div className="card-lower__wrapper">
+                  <FontAwesomeIcon
+                    icon={faUsers}
+                    className="card-lower__users-icon"
+                  />
                 </div>
               </div>
-              <div className="card-lower__wrapper">
-                <FontAwesomeIcon
-                  icon={faUsers}
-                  className="card-lower__users-icon"
-                />
+              <div className="card-lower-border--black">
+                <div className="card-lower-icon-value">
+                  {studentsGrowthState ? (
+                    <span className="card-lower-loading">Loading...</span>
+                  ) : (
+                    <div>
+                      {studentsGrowth.growth_rate > 0 ? (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowUp}
+                            className="card-arrow-up-icon" // Up arrow
+                          />
+                          <span className="growth-rate-positive">
+                            {studentsGrowth.growth_rate}%{" "}
+                          </span>
+                        </>
+                      ) : (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowDown}
+                            className="card-arrow-down-icon" // Down arrow
+                          />
+                          <span className="growth-rate-negative">
+                            {studentsGrowth.growth_rate}%{" "}
+                          </span>
+                        </>
+                      )}
+                    </div>
+                  )}
+                </div>
+                <span className="card-lower-text">increased this month</span>
               </div>
-            </div>
-            <div className="card-lower-border--black">
-              <div className="card-lower-icon-value">
-                {studentsGrowthState ? (
-                  <span className="card-lower-loading">Loading...</span>
-                ) : (
-                  <div>
-                    {studentsGrowth.growth_rate > 0 ? (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowUp}
-                          className="card-arrow-up-icon" // Up arrow
-                        />
-                        <span className="growth-rate-positive">
-                          {studentsGrowth.growth_rate}%{" "}
-                        </span>
-                      </>
-                    ) : (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowDown}
-                          className="card-arrow-down-icon" // Down arrow
-                        />
-                        <span className="growth-rate-negative">
-                          {studentsGrowth.growth_rate}%{" "}
-                        </span>
-                      </>
-                    )}
+            </Card>
+          </div>
+          <div className="cards-container__inner-box">
+            <Card className="card card__card--blue">
+              <div className="card-upper">
+                <div className="card-upper__wrapper">
+                  <div className="card-upper__upper">Registered Users</div>
+                  <div className="card-upper__lower">
+                    {countRegisteredUsersState ? null : countRegisteredUsers}
                   </div>
-                )}
+                </div>
+                <div className="card-lower__wrapper">
+                  <FontAwesomeIcon
+                    icon={faFile}
+                    className="card-lower__users-icon"
+                  />
+                </div>
               </div>
-              <span className="card-lower-text">increased this month</span>
-            </div>
-          </Card>
+              <div className="card-lower-border--blue">
+                <div className="card-lower-icon-value">
+                  {registeredUsersGrowthState ? (
+                    <span className="card-lower-loading">Loading...</span>
+                  ) : (
+                    <div>
+                      {registeredUsersGrowth.growth_rate > 0 ? (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowUp}
+                            className="card-arrow-up-icon" // Up arrow
+                          />
+                          <span className="growth-rate-positive">
+                            {registeredUsersGrowth.growth_rate}%{" "}
+                          </span>
+                        </>
+                      ) : (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowDown}
+                            className="card-arrow-down-icon" // Down arrow
+                          />
+                          <span className="growth-rate-negative">
+                            {registeredUsersGrowth.growth_rate}%{" "}
+                          </span>
+                        </>
+                      )}
+                    </div>
+                  )}
+                </div>
+                <span className="card-lower-text">increased this month</span>
+              </div>
+            </Card>
+          </div>
         </div>
-        <div className="cards-container__inner-box">
-          <Card className="card card__card--blue">
-            <div className="card-upper">
-              <div className="card-upper__wrapper">
-                <div className="card-upper__upper">Registered Users</div>
-                <div className="card-upper__lower">
-                  {countRegisteredUsersState ? null : countRegisteredUsers}
-                </div>
-              </div>
-              <div className="card-lower__wrapper">
-                <FontAwesomeIcon
-                  icon={faFile}
-                  className="card-lower__users-icon"
-                />
-              </div>
-            </div>
-            <div className="card-lower-border--blue">
-              <div className="card-lower-icon-value">
-                {registeredUsersGrowthState ? (
-                  <span className="card-lower-loading">Loading...</span>
-                ) : (
-                  <div>
-                    {registeredUsersGrowth.growth_rate > 0 ? (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowUp}
-                          className="card-arrow-up-icon" // Up arrow
-                        />
-                        <span className="growth-rate-positive">
-                          {registeredUsersGrowth.growth_rate}%{" "}
-                        </span>
-                      </>
-                    ) : (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowDown}
-                          className="card-arrow-down-icon" // Down arrow
-                        />
-                        <span className="growth-rate-negative">
-                          {registeredUsersGrowth.growth_rate}%{" "}
-                        </span>
-                      </>
-                    )}
+        <div className="">
+          <div className="cards-container__inner-box">
+            <Card className="card card__card--red">
+              <div className="card-upper">
+                <div className="card-upper__wrapper">
+                  <div className="card-upper__upper">
+                    Pending credentials approval
                   </div>
-                )}
-              </div>
-              <span className="card-lower-text">increased this month</span>
-            </div>
-          </Card>
-        </div>
-        <div className="cards-container__inner-box">
-          <Card className="card card__card--red">
-            <div className="card-upper">
-              <div className="card-upper__wrapper">
-                <div className="card-upper__upper">
-                  Pending credentials approval
-                </div>
-                <div className="card-upper__lower">
-                  {countPendingCredentialsUsersState
-                    ? null
-                    : countPendingCredentialsUsers}
-                </div>
-              </div>
-              <div className="card-lower__wrapper">
-                <FontAwesomeIcon
-                  icon={faHourglassHalf}
-                  className="card-lower__users-icon"
-                />
-              </div>
-            </div>
-            <div className="card-lower-border--red">
-              <div className="card-lower-icon-value">
-                {countPendingCredentialsUsersGrowthRateState ? (
-                  <span className="card-lower-loading">Loading...</span>
-                ) : (
-                  <div>
-                    {countPendingCredentialsUsersGrowthRate.growth_rate > 0 ? (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowUp}
-                          className="card-arrow-up-icon" // Up arrow
-                        />
-                        <span className="growth-rate-positive">
-                          {countPendingCredentialsUsersGrowthRate.growth_rate}%{" "}
-                        </span>
-                      </>
-                    ) : (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowDown}
-                          className="card-arrow-down-icon" // Down arrow
-                        />
-                        <span className="growth-rate-negative">
-                          {countPendingCredentialsUsersGrowthRate.growth_rate}%{" "}
-                        </span>
-                      </>
-                    )}
+                  <div className="card-upper__lower">
+                    {countPendingCredentialsUsersState
+                      ? null
+                      : countPendingCredentialsUsers}
                   </div>
-                )}
-              </div>
-              <span className="card-lower-text">increased this month</span>
-            </div>
-          </Card>
-        </div>
-        <div className="cards-container__inner-box">
-          <Card className="card card__card--green">
-            {" "}
-            <div className="card-upper">
-              <div className="card-upper__wrapper">
-                <div className="card-upper__upper">
-                  Pending guarantors approval
                 </div>
-                <div className="card-upper__lower">
-                  {countPendingCredentialsUsersState
-                    ? null
-                    : countPendingCredentialsUsers}
+                <div className="card-lower__wrapper">
+                  <FontAwesomeIcon
+                    icon={faHourglassHalf}
+                    className="card-lower__users-icon"
+                  />
                 </div>
               </div>
-              <div className="card-lower__wrapper">
-                <FontAwesomeIcon
-                  icon={faHourglassHalf}
-                  className="card-lower__users-icon"
-                />
+              <div className="card-lower-border--red">
+                <div className="card-lower-icon-value">
+                  {countPendingCredentialsUsersGrowthRateState ? (
+                    <span className="card-lower-loading">Loading...</span>
+                  ) : (
+                    <div>
+                      {countPendingCredentialsUsersGrowthRate.growth_rate >
+                      0 ? (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowUp}
+                            className="card-arrow-up-icon" // Up arrow
+                          />
+                          <span className="growth-rate-positive">
+                            {countPendingCredentialsUsersGrowthRate.growth_rate}
+                            %{" "}
+                          </span>
+                        </>
+                      ) : (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowDown}
+                            className="card-arrow-down-icon" // Down arrow
+                          />
+                          <span className="growth-rate-negative">
+                            {countPendingCredentialsUsersGrowthRate.growth_rate}
+                            %{" "}
+                          </span>
+                        </>
+                      )}
+                    </div>
+                  )}
+                </div>
+                <span className="card-lower-text">increased this month</span>
               </div>
-            </div>
-            <div className="card-lower-border--green">
-              <div className="card-lower-icon-value">
-                {countPendingCredentialsUsersGrowthRateState ? (
-                  <span className="card-lower-loading">Loading...</span>
-                ) : (
-                  <div>
-                    {countPendingCredentialsUsersGrowthRate.growth_rate > 0 ? (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowUp}
-                          className="card-arrow-up-icon" // Up arrow
-                        />
-                        <span className="growth-rate-positive">
-                          {countPendingCredentialsUsersGrowthRate.growth_rate}%{" "}
-                        </span>
-                      </>
-                    ) : (
-                      <>
-                        <FontAwesomeIcon
-                          icon={faArrowDown}
-                          className="card-arrow-down-icon" // Down arrow
-                        />
-                        <span className="growth-rate-negative">
-                          {countPendingCredentialsUsersGrowthRate.growth_rate}%{" "}
-                        </span>
-                      </>
-                    )}
+            </Card>
+          </div>
+          <div className="cards-container__inner-box">
+            <Card className="card card__card--green">
+              {" "}
+              <div className="card-upper">
+                <div className="card-upper__wrapper">
+                  <div className="card-upper__upper">
+                    Pending guarantors approval
                   </div>
-                )}
+                  <div className="card-upper__lower">
+                    {countPendingCredentialsUsersState
+                      ? null
+                      : countPendingCredentialsUsers}
+                  </div>
+                </div>
+                <div className="card-lower__wrapper">
+                  <FontAwesomeIcon
+                    icon={faHourglassHalf}
+                    className="card-lower__users-icon"
+                  />
+                </div>
               </div>
-              <span className="card-lower-text">increased this month</span>
-            </div>
-          </Card>
+              <div className="card-lower-border--gred">
+                <div className="card-lower-icon-value">
+                  {countPendingCredentialsUsersGrowthRateState ? (
+                    <span className="card-lower-loading">Loading...</span>
+                  ) : (
+                    <div>
+                      {countPendingCredentialsUsersGrowthRate.growth_rate >
+                      0 ? (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowUp}
+                            className="card-arrow-up-icon" // Up arrow
+                          />
+                          <span className="growth-rate-positive">
+                            {countPendingCredentialsUsersGrowthRate.growth_rate}
+                            %{" "}
+                          </span>
+                        </>
+                      ) : (
+                        <>
+                          <FontAwesomeIcon
+                            icon={faArrowDown}
+                            className="card-arrow-down-icon" // Down arrow
+                          />
+                          <span className="growth-rate-negative">
+                            {countPendingCredentialsUsersGrowthRate.growth_rate}
+                            %{" "}
+                          </span>
+                        </>
+                      )}
+                    </div>
+                  )}
+                </div>
+                <span className="card-lower-text">increased this month</span>
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </main>
